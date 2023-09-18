62670d3a91ee51fc72d59504a5963137e4a8587f
EderBraganca <eder.braganca@aluno.ufop.edu.br>
2023-09-18 13:08:03 -0300

    up up up up

--- Questao9_Employes.sql
+++ Questao9_Employes.sql
@@ -3,7 +3,7 @@
 --partition by departament 
 --count departament
 --avarage salario baseado no dp
-
+    
 SELECT departament, name, last_name, salary,
 RANK() OVER (PARTITION BY departament ORDER BY salary DESC) AS ranking,
 COUNT(*) OVER (PARTITION BY departament) AS total_funcionarios, 
